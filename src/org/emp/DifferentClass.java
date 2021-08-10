d3fe84ef92580947ae0f6e97731808068fd4fa07
Aji2324 <ajithece23@gmail.com>
2021-08-10 18:24:33 +0530

    my first push

---
+++ src/org/emp/DifferentClass.java
@@ -0,0 +1,13 @@
+package org.emp;
+
+public class DifferentClass{
+
+	public static void main(String[] args) {
+		Staticvariable.empId();
+		
+	}
+	
+	
+	
+	
+}
---
+++ src/org/emp/Staticvariable.java
@@ -0,0 +1,15 @@
+package org.emp;
+
+public class Staticvariable {
+
+	 
+	public static  void empId() {
+		System.out.println("Print the letter");
+	}
+	public static void compId() {
+		System.out.println("Not Print");
+	}
+
+				
+	
+}
